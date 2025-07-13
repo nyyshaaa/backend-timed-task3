7f63d2e58bc99d84f1f41c6302d292293529433a
nisha <2016ume1288@mnit.ac.in>
2025-07-13 12:30:58 +0530

    pass redirect uri directly

--- src/spotify/auth.py
+++ src/spotify/auth.py
@@ -10,7 +10,7 @@ async def get_access_token(code: str):
             data={
                 "grant_type": "authorization_code",
                 "code": code,
-                "redirect_uri": settings.SPOTIPY_REDIRECT_URI,
+                "redirect_uri": "http://127.0.0.1:8000/auth/spotify/callback",
                 "client_id": settings.SPOTIPY_CLIENT_ID,
                 "client_secret": settings.SPOTIPY_CLIENT_SECRET,
             },
@@ -18,4 +18,23 @@ async def get_access_token(code: str):
         )
     if response.status_code != 200:
         raise HTTPException(status_code=response.status_code, detail=response.json())
+    return response.json()
+
+async def refresh_access_token(refresh_token: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            "https://accounts.spotify.com/api/token",
+            # form-encoded body:
+            data={
+                "grant_type": "refresh_token",
+                "refresh_token": refresh_token,
+            },
+            # HTTP Basic auth: client_id:client_secret
+            auth=(settings.SPOTIPY_CLIENT_ID, settings.SPOTIPY_CLIENT_SECRET),
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail=response.json())
+
     return response.json()
